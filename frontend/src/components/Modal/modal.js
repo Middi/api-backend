f618ac7add9bc8d57787c2c76de26b86806c147c
“Richard Middleton” <Richard@richardmiddleton.me>
2018-07-21 13:05:13 +0200

    changed to stateless

--- frontend/src/components/Modal/modal.js
+++ frontend/src/components/Modal/modal.js
@@ -1,38 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './style.css';
 
 
-class Modal extends Component {
-    render() {
-        return (
-            <div className="modal-bg">
-                <div className="modal">
-                    <span onClick={() => this.props.closeModal()} className="close">+</span>
-                    <h1 className="event-title">Add Event</h1>
-                    <form>
-                        <label>Event</label>
-                        <input type="text" name="name" onKeyUp={e => this.props.change(e)} />
-                        <label>Location</label>
-                        <input type="text" name="location" onKeyUp={e => this.props.change(e)} />
-                        <label>Start Date</label>
-                        <input type="date" name="dateStart" onKeyUp={e => this.props.change(e)} />
-                        <label>Start Time</label>
-                        <input type="time" name="timeStart" onKeyUp={e => this.props.change(e)} />
-                        <label>End Date</label>
-                        <input type="date" name="dateEnd" onKeyUp={e => this.props.change(e)} />
-                        <label>End Time</label>
-                        <input type="time" name="timeEnd" onKeyUp={e => this.props.change(e)} />
-                        <label>Shift Type</label>
-                        <select name="shiftType" onChange={e => this.props.change(e)}>
-                            <option value="1000" name="shiftType">Full Day</option>
-                            <option value="500" name="shiftType">Half Day</option>
-                        </select>
-                        <button onClick={(e) => this.props.clickSubmit(e)}>Submit</button>
-                    </form>
-                </div>
-            </div>
-        )
-    }
-}
+const Modal = props => (
+    <div className="modal-bg">
+        <div className="modal">
+            <span onClick={() => props.closeModal()} className="close">+</span>
+            <h1 className="event-title">Add Event</h1>
+            <form>
+                <label>Event</label>
+                <input type="text" name="name" onKeyUp={e => props.change(e)} />
+                <label>Location</label>
+                <input type="text" name="location" onKeyUp={e => props.change(e)} />
+                <label>Start Date</label>
+                <input type="date" name="dateStart" onKeyUp={e => props.change(e)} />
+                <label>Start Time</label>
+                <input type="time" name="timeStart" onKeyUp={e => props.change(e)} />
+                <label>End Date</label>
+                <input type="date" name="dateEnd" onKeyUp={e => props.change(e)} />
+                <label>End Time</label>
+                <input type="time" name="timeEnd" onKeyUp={e => props.change(e)} />
+                <label>Shift Type</label>
+                <select name="shiftType" onChange={e => props.change(e)}>
+                    <option value="1000" name="shiftType">Full Day</option>
+                    <option value="500" name="shiftType">Half Day</option>
+                </select>
+                <button onClick={(e) => props.clickSubmit(e)}>Submit</button>
+            </form>
+        </div>
+    </div>
+)
 
 export default Modal;
--- frontend/src/components/Monthly/index.js
+++ frontend/src/components/Monthly/index.js
@@ -1,39 +1,41 @@
-import React, { Component } from 'react';
-import Day from '../Day';
-import './style.css';
+import React from "react";
+import Day from "../Day";
+import "./style.css";
 
-class Monthly extends Component {
-    
-    render() {
-        let arr = [];
-        for(var i= (2 - this.props.startDay); i<this.props.daysInMonth+1; i++){
-            arr.push(
-                <Day addEvent={this.props.addEvent} date={i} key={i} today={this.props.today} />
-            );  
-        }
-        return (
-            <div className="month">
-                <div className="title-month">
-                    <h2 onClick={this.props.monthMinus}>&lt;</h2>
-                    <h1>{this.props.monthName}</h1>
-                    <h2>&gt;</h2>
-                </div>
-                <div className="calendar">
-                    <div className="calendar-header">
-                        <p className="day">M</p>
-                        <p className="day">T</p>
-                        <p className="day">W</p>
-                        <p className="day">T</p>
-                        <p className="day">F</p>
-                        <p className="day">S</p>
-                        <p className="day">S</p>
+const Monthly = props => {
+  let arr = [];
+  for (var i = 2 - props.startDay; i < props.daysInMonth + 1; i++) {
+    arr.push(
+      <Day
+        addEvent={props.addEvent}
+        date={i}
+        key={i}
+        today={props.today}
+      />
+    );
+  }
+  return (
+    <div className="month">
+      <div className="title-month">
+        <h2 onClick={props.monthMinus}>&lt;</h2>
+        <h1>{props.monthName}</h1>
+        <h2>&gt;</h2>
+      </div>
+      <div className="calendar">
+        <div className="calendar-header">
+          <p className="day">M</p>
+          <p className="day">T</p>
+          <p className="day">W</p>
+          <p className="day">T</p>
+          <p className="day">F</p>
+          <p className="day">S</p>
+          <p className="day">S</p>
 
-                        {arr}
-                    </div>
-                </div>
-            </div>
-        )
-    }
-}
+          {arr}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Monthly;
